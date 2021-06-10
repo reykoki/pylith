0563a3f8e5f411b7669f2123b6ac7ee9625ace9b
reykoki <reykokimonster@gmail.com>
2021-06-10 14:26:31 -0600

    taking out filename

--- pylith/meshio/MeshIOPETSc.py
+++ pylith/meshio/MeshIOPETSc.py
@@ -24,32 +24,14 @@ from .MeshIOObj import MeshIOObj
 from .meshio import MeshIOPETSc as ModuleMeshIOPETSc
 
 
-def validateFilename(value):
-    """Validate filename.
-    """
-    if 0 == len(value):
-        msg = "Filename for gmsh input mesh not specified.  " + \
-            "To test PyLith, run an example as discussed in the manual."
-        raise ValueError(msg)
-    try:
-        open(value, "r")
-    except IOError:
-        raise IOError("gmsh input mesh '{}' not found.".format(value))
-    return value
-
-
 class MeshIOPETSc(MeshIOObj, ModuleMeshIOPETSc):
     """Python object for reading/writing finite-element mesh from simple
-    gmsh file.
+    PETSc file.
 
     Factory: mesh_io
     """
 
     import pythia.pyre.inventory
-
-    filename = pythia.pyre.inventory.str("filename", default="",
-                                  validator=validateFilename)
-    filename.meta['tip'] = "Name of mesh file"
 
     from spatialdata.geocoords.CSCart import CSCart
     coordsys = pythia.pyre.inventory.facility("coordsys", family="coordsys",
@@ -68,7 +50,6 @@ class MeshIOPETSc(MeshIOObj, ModuleMeshIOPETSc):
         """Do minimal initialization."""
         MeshIOObj.preinitialize(self)
 
-        ModuleMeshIOPETSc.filename(self, self.filename)
         return
 
     # PRIVATE METHODS ////////////////////////////////////////////////////
